0cef68b124544d59cb2c90620e65c5511cd6ca7d
Gabriel Creti <me@kaboo.boo>
2023-01-20 21:28:21 +0100

    Droidcam script functions

--- scripts/cam.sh
+++ scripts/cam.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+
+source ~/.secrets/credentials.sh
+
 function unlock() {
   adb shell input keyevent 26
 }
+LOCAL_NET_CIDR="$(ip route | grep src | grep wlp0s20f3 | awk '{print $1}')"
+
+# Scan network to find phone
+nmap -sn $LOCAL_NET_CIDR
+
+echo $PHONE_MAC
 
 PHONE_IP="$(arp -a | grep $PHONE_MAC | sed -nr 's/.*\((.+)\).*/\1/p')"
 
@@ -16,6 +26,12 @@ adb shell am start -n com.dev47apps.droidcamx/.DroidCamX
 
 sleep 2
 
-droidcam-cli adb 4747
-
-adb shell input keyevent 26
+# Start droidcam infinitely until it starts properly
+while true; do
+  droidcam-cli -nocontrols adb 4747 2>&1 | while read LOGLINE
+  do
+    [[ "${LOGLINE}" == *"Error: Connection reset!"* ]] && pkill -P $$ droidcam-cli
+  done
+  sleep 2
+  echo "Droidcam failed (Error: Connection reset!). Restarting!"
+done
