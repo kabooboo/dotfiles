b6b7cf50729b51c572603b71e64a30917139dd34
Gabriel Creti <me@kaboo.boo>
2024-01-18 16:45:20 +0100

    checkpoint

--- screenlayout/autoxrandr.sh
+++ screenlayout/autoxrandr.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 if [ "$(cat /proc/acpi/button/lid/LID0/state | awk '{print $2}')" == "open" ]; then
-  EDP1_STRING="--auto --pos 0x1080 --rotate normal"
+  EDP1_STRING="--auto --rotate normal"
 else
   EDP1_STRING="--off"
 fi
 
 if [ "$(xrandr  | grep -E '^HDMI-1' | grep ' connected ' | wc -l)" == "1" ]; then
-  HDMI1_STRING="--auto --pos 0x0 --rotate normal"
+  HDMI1_STRING="--auto --pos 0x0 --rotate normal --above eDP-1"
 else
   HDMI1_STRING="--off"
 fi
 
 if [ "$(xrandr  | grep -E '^HDMI-2' | grep ' connected ' | wc -l)" == "1" ]; then
-  HDMI2_STRING="--auto --pos 0x0 --rotate normal"
+  HDMI2_STRING="--auto --pos 0x0 --rotate normal --above eDP-1"
 else
   HDMI2_STRING="--off"
 fi
 
 if [ "$(xrandr  | grep -E '^HDMI-3' | grep ' connected ' | wc -l)" == "1" ]; then
-  HDMI3_STRING="--auto --pos 0x0 --rotate normal"
+  HDMI3_STRING="--auto --pos 0x0 --rotate normal --above eDP-1"
 else
   HDMI3_STRING="--off"
 fi
 
 if [ "$(xrandr  | grep -E '^DP-1' | grep ' connected ' | wc -l)" == "1" ]; then
-  DP1_STRING="--auto --pos 0x0 --rotate normal"
+  DP1_STRING="--auto --pos 0x0 --rotate normal --above eDP-1"
 else
   DP1_STRING="--off"
 fi
 
 if [ "$(xrandr  | grep -E '^DP-2' | grep ' connected ' | wc -l)" == "1" ]; then
-  DP2_STRING="--auto --pos 0x0 --rotate normal"
+  DP2_STRING="--auto --pos 0x0 --rotate normal --above eDP-1"
 else
   DP2_STRING="--off"
 fi
